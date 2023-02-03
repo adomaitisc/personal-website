bca0f90f9b953b182a86aed8d274bd5eb6e2d52a
adomaitisc <caadomaitis@gmail.com>
2023-02-02 23:29:55 -0500

    bg color changed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,22 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            color-scheme: dark;
+          }
+
+          body {
+            background-color: #000;
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
