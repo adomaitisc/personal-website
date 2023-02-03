f4f7232b5041de3000ec2cc3ee07f90a835c5ec5
adomaitisc <caadomaitis@gmail.com>
2023-02-02 19:44:26 -0500

    fixed apostrophe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
           </div>
           <div className="flex w-[366px] items-center gap-2 rounded-2xl border border-white/10 bg-black p-2 text-white">
             <Image src={warhaus} alt="album of the week" />
-            <p className="text-sm font-medium">Warhaus - Love's a Stranger</p>
+            <p className="text-sm font-medium">{`Warhaus - Love's a Stranger`}</p>
           </div>
           <div className="flex w-[366px] flex-col rounded-2xl border border-white/10 bg-black p-6 text-white">
             <Spline
